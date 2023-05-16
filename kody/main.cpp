2c4340741c118bc67a7aee34d3da4ca50edc2df9
Marek Wala <kontakt@twc.company>
2023-05-16 18:16:07 +0200

    Initial Commit

---
+++ kody/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  kody
+//
+//  Created by Marek Wala on 16/05/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
